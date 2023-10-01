1419b1d22457d54626f6776b53022109dcbbbd7a
lucas-afonso <lucas.afonso@sempreceub.com>
2023-09-30 23:52:20 -0300

    Update SolicitarCuidado1.dart

--- cuidadoamigoapp/lib/views/SolicitarCuidado1.dart
+++ cuidadoamigoapp/lib/views/SolicitarCuidado1.dart
@@ -11,8 +11,8 @@ class SolicitarCuidado1 extends StatefulWidget {
 class _SolicitarCuidado1State extends State<SolicitarCuidado1> {
   bool _exibirCamposEndereco = false;
   TimeOfDay selectedTime = TimeOfDay.now();
-  var HoraInicio = "inicio";
-  var Horafim = "fim";
+  var HoraInicio = "Início";
+  var Horafim = "Fim";
   TextEditingController dataController = TextEditingController();
 
   @override
@@ -36,112 +36,166 @@ class _SolicitarCuidado1State extends State<SolicitarCuidado1> {
               ),
               // Campo de data
               TextFormField(
-                controller:dataController,
+                controller: dataController,
                 keyboardType: TextInputType.datetime,
                 decoration: InputDecoration(
                   hintText: 'Selecione a data',
+                  prefixIcon: Icon(Icons.date_range),
                 ),
-                onTap: () async{
-                  DateTime? pickddate = await showDatePicker(
-                        context: context,
-                        initialDate: DateTime.now(),
-                        firstDate: DateTime(2000),
-                        lastDate: DateTime(2101));
-                  if (pickddate != null) {
-                      setState(() {
-                        dataController.text =
-                            DateFormat('dd/MM/yyyy').format(pickddate);
-                      });
-                    };
-              }),
+                onTap: () async {
+                  DateTime? pickedDate = await showDatePicker(
+                    context: context,
+                    initialDate: DateTime.now(),
+                    firstDate: DateTime(2000),
+                    lastDate: DateTime(2101),
+                  );
+                  if (pickedDate != null) {
+                    setState(() {
+                      dataController.text =
+                          DateFormat('dd/MM/yyyy').format(pickedDate);
+                    });
+                  }
+                },
+              ),
               SizedBox(height: 20),
+              Text(
+                'Horário',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
               Row(
                 children: [
-                  ElevatedButton(
-                    onPressed: ()  async {
-                          final TimeOfDay? timeOfDay = await showTimePicker(
-                            context: context,
-                            initialTime: selectedTime,
-                            initialEntryMode: TimePickerEntryMode.dial,
-                          );
-                          if (TimeOfDay != null) {
-                            setState(() {
-                              HoraInicio =
-                                  timeOfDay!.format(context).characters.string;
-                            });
-                          };
-                        },
-                  
-                    style: ElevatedButton.styleFrom(
-                      primary: Color(0xFF73C9C9), // Cor do botão
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(30.0), // Torna o botão redondo
+                  Expanded(
+                    child: ElevatedButton(
+                      onPressed: () async {
+                        final TimeOfDay? timeOfDay = await showTimePicker(
+                          context: context,
+                          initialTime: selectedTime,
+                          initialEntryMode: TimePickerEntryMode.dial,
+                        );
+                        if (timeOfDay != null) {
+                          setState(() {
+                            HoraInicio = timeOfDay.format(context);
+                          });
+                        }
+                      },
+                      style: ElevatedButton.styleFrom(
+                        primary: Color(0xFF73C9C9),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(30.0),
+                        ),
                       ),
-                    ),
-                    child: Text('${HoraInicio}',
-                      style: TextStyle(color: Colors.white),
+                      child: Padding(
+                        padding: EdgeInsets.symmetric(vertical: 12.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              'Início',
+                              style: TextStyle(color: Colors.white),
+                            ),
+                            SizedBox(width: 4.0),
+                            Icon(Icons.access_time, color: Colors.white),
+                          ],
+                        ),
+                      ),
                     ),
                   ),
                   SizedBox(width: 10),
-                  ElevatedButton(
-                    onPressed: ()  async {
-                          final TimeOfDay? timeOfDay = await showTimePicker(
-                            context: context,
-                            initialTime: selectedTime,
-                            initialEntryMode: TimePickerEntryMode.dial,
-                          );
-                          if (TimeOfDay != null) {
-                            setState(() {
-                              Horafim =
-                                  timeOfDay!.format(context).characters.string;
-                            });
-                          };
-                        },
-                    style: ElevatedButton.styleFrom(
-                      primary: Color(0xFF73C9C9), // Cor do botão
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(30.0), // Torna o botão redondo
-                      ),
+                  Text(
+                    'até',
+                    style: TextStyle(
+                      fontSize: 18.0,
                     ),
-                    child: Text(
-                      '${Horafim}',
-                      style: TextStyle(color: Colors.white),
+                  ),
+                  SizedBox(width: 10),
+                  Expanded(
+                    child: ElevatedButton(
+                      onPressed: () async {
+                        final TimeOfDay? timeOfDay = await showTimePicker(
+                          context: context,
+                          initialTime: selectedTime,
+                          initialEntryMode: TimePickerEntryMode.dial,
+                        );
+                        if (timeOfDay != null) {
+                          setState(() {
+                            Horafim = timeOfDay.format(context);
+                          });
+                        }
+                      },
+                      style: ElevatedButton.styleFrom(
+                        primary: Color(0xFF73C9C9),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(30.0),
+                        ),
+                      ),
+                      child: Padding(
+                        padding: EdgeInsets.symmetric(vertical: 12.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              'Fim',
+                              style: TextStyle(color: Colors.white),
+                            ),
+                            SizedBox(width: 4.0),
+                            Icon(Icons.access_time, color: Colors.white),
+                          ],
+                        ),
+                      ),
                     ),
                   ),
                 ],
               ),
               SizedBox(height: 20),
+              Text(
+                'Endereço',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
               Row(
                 children: [
-                  ElevatedButton(
-                    onPressed: () {},
-                    style: ElevatedButton.styleFrom(
-                      primary: Color(0xFF73C9C9), // Cor do botão
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(30.0), // Torna o botão redondo
+                  Expanded(
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      style: ElevatedButton.styleFrom(
+                        primary: Color(0xFF73C9C9),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20.0),
+                        ),
                       ),
-                    ),
-                    child: Text(
-                      'Salvo',
-                      style: TextStyle(color: Colors.white),
+                      child: Padding(
+                        padding: EdgeInsets.symmetric(vertical: 12.0),
+                        child: Text(
+                          'Salvo',
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      ),
                     ),
                   ),
                   SizedBox(width: 10),
-                  ElevatedButton(
-                    onPressed: () {
-                      setState(() {
-                        _exibirCamposEndereco = !_exibirCamposEndereco;
-                      });
-                    },
-                    style: ElevatedButton.styleFrom(
-                      primary: Color(0xFF73C9C9), // Cor do botão
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(30.0), // Torna o botão redondo
+                  Expanded(
+                    child: ElevatedButton(
+                      onPressed: () {
+                        setState(() {
+                          _exibirCamposEndereco = !_exibirCamposEndereco;
+                        });
+                      },
+                      style: ElevatedButton.styleFrom(
+                        primary: Color(0xFF73C9C9),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20.0),
+                        ),
                       ),
-                    ),
-                    child: Text(
-                      'Novo',
-                      style: TextStyle(color: Colors.white),
+                      child: Padding(
+                        padding: EdgeInsets.symmetric(vertical: 12.0),
+                        child: Text(
+                          'Novo',
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      ),
                     ),
                   ),
                 ],
@@ -150,29 +204,34 @@ class _SolicitarCuidado1State extends State<SolicitarCuidado1> {
                 Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
+                    SizedBox(height: 20),
                     TextFormField(
                       // Campo de CEP
                       keyboardType: TextInputType.number,
                       decoration: InputDecoration(
                         hintText: 'CEP',
+                        prefixIcon: Icon(Icons.location_on),
                       ),
                     ),
                     TextFormField(
                       // Campo de Cidade
                       decoration: InputDecoration(
                         hintText: 'Cidade',
+                        prefixIcon: Icon(Icons.location_city),
                       ),
                     ),
                     TextFormField(
                       // Campo de Endereço
                       decoration: InputDecoration(
                         hintText: 'Endereço',
+                        prefixIcon: Icon(Icons.home),
                       ),
                     ),
                     TextFormField(
                       // Campo de Complemento
                       decoration: InputDecoration(
                         hintText: 'Complemento (opcional)',
+                        prefixIcon: Icon(Icons.add),
                       ),
                     ),
                     TextFormField(
@@ -180,24 +239,34 @@ class _SolicitarCuidado1State extends State<SolicitarCuidado1> {
                       keyboardType: TextInputType.number,
                       decoration: InputDecoration(
                         hintText: 'Número',
+                        prefixIcon: Icon(Icons.format_list_numbered),
                       ),
                     ),
                   ],
                 ),
               SizedBox(height: 20),
-              ElevatedButton(
-                onPressed: () {
-                  Navigator.of(context).pushNamed('/solicitarCuidador2');
-                },
-                style: ElevatedButton.styleFrom(
-                  primary: Color(0xFF73C9C9), // Cor do botão
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(30.0), // Torna o botão redondo
+              Align(
+                alignment: Alignment.bottomCenter,
+                child: ElevatedButton(
+                  onPressed: () {
+                    Navigator.of(context).pushNamed('/solicitarCuidador2');
+                  },
+                  style: ElevatedButton.styleFrom(
+                    primary: Color(0xFF73C9C9),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30.0),
+                    ),
                   ),
-                ),
-                child: Text(
-                  'Prosseguir',
-                  style: TextStyle(color: Colors.white),
+                  child: Padding(
+                    padding: EdgeInsets.symmetric(vertical: 16.0, horizontal: 40.0),
+                    child: Text(
+                      'Prosseguir',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
                 ),
               ),
             ],
