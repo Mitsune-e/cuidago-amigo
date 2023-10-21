9864f0f6eb43c4bb750b81e4925014e15e026077
edersonjr <ederson.junior@sempreceub.com>
2023-10-21 11:30:57 -0300

    Implementacao autorizacao e login incompleta

---
+++ cuidadoamigoapp/lib/servicos/autorizacao.dart
@@ -0,0 +1,17 @@
+class AuthService extends ChangeNotifier {
+  FirebaseAuth _auth = FirebaseAuth.instance;
+  User? usuario;
+  bool isLoading = true;
+  
+}
+  AuthService() {
+    _authCheck();
+  }
+
+  _authCheck() {
+    _auth.authStateChanges().listen((User? user) {
+      usuario = (user == null) ? null : user;
+      isLoading = false;
+      notifyListeners();
+    });
+  }
