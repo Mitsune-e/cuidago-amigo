c6e6457c5152738ddb995215cc84764856fa29d6
Mitsune-e <gvelosodev@gmail.com>
2024-12-16 17:51:57 -0300

    Fixing package name problems and last fixes before showcase

---
+++ cuidadoamigoapp/lib/provider/Clientes.dart
@@ -0,0 +1,97 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:cuidadoamigoapp/models/cliente.dart';
+import 'package:flutter/material.dart';
+
+class Clientes with ChangeNotifier {
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+  List<Cliente> listEvento = [];
+
+  void adiciona(Cliente cliente) {
+    _firestore.collection("Clientes").doc(cliente.id).set(cliente.toMap());
+  }
+
+  void editar(Cliente cliente) {
+    // Certifique-se de que o endereço já existe na lista
+    //int index = listEvento.indexWhere((e) => e.id == cliente.id);
+
+    //if (index != -1) {
+    // Atualiza a lista local
+    //listEvento[index] = cliente;
+
+    // Atualiza os dados no Firestore
+    _firestore.collection("Clientes").doc(cliente.id).set(cliente.toMap());
+
+    notifyListeners();
+    //}
+  }
+
+  Future<List<Cliente>> caregar() async {
+    List<Cliente> temp = [];
+    QuerySnapshot<Map<String, dynamic>> snapshot =
+        await _firestore.collection('Clientes').get();
+    for (var element in snapshot.docs) {
+      temp.add(Cliente.fromMap(element.data()));
+    }
+    return temp;
+  }
+
+  Future<Cliente> caregarById(String id) async {
+    QuerySnapshot<Map<String, dynamic>> snapshot =
+        await _firestore.collection('Clientes').get();
+
+    final dadosCliente = snapshot.docs.firstWhere((item) => item.id == id);
+
+    final clienteSelecionado = Cliente.fromMap(dadosCliente.data());
+    return clienteSelecionado;
+  }
+
+  void remove(Cliente cliente) {
+    _firestore.collection('Clientes').doc(cliente.id).delete();
+    notifyListeners();
+  }
+
+  void removeById(String clienteId) {
+    // Remove do Firestore
+    _firestore.collection('Clientes').doc(clienteId).delete();
+
+    // Remove da lista local
+    listEvento.removeWhere((cliente) => cliente.id == clienteId);
+
+    // Notifica os ouvintes que a lista foi alterada
+    notifyListeners();
+  }
+
+  Cliente? loadClienteByIdSync(String clienteId) {
+    try {
+      DocumentSnapshot<Map<String, dynamic>> snapshot = _firestore
+          .collection('Clientes')
+          .doc(clienteId)
+          .get() as DocumentSnapshot<Map<String, dynamic>>;
+
+      if (snapshot.exists) {
+        return Cliente.fromMap(snapshot.data()!);
+      } else {
+        return null;
+      }
+    } catch (e) {
+      print('Erro ao carregar cliente: $e');
+      return null;
+    }
+  }
+
+  Future<Cliente?> loadClienteById(String clientId) async {
+    try {
+      DocumentSnapshot<Map<String, dynamic>> snapshot =
+          await _firestore.collection('Clientes').doc(clientId).get();
+
+      if (snapshot.exists) {
+        return Cliente.fromMap(snapshot.data()!);
+      } else {
+        return null;
+      }
+    } catch (e) {
+      print('Erro ao carregar cliente: $e');
+      return null;
+    }
+  }
+}
---
+++ cuidadoamigoapp/lib/provider/servicos.dart
@@ -0,0 +1,97 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:cuidadoamigoapp/models/Servico.dart';
+import 'package:flutter/material.dart';
+
+class Servicos with ChangeNotifier {
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+  List<Servico> listEvento = [];
+  bool isLoading = false; // Adiciona a flag isLoading
+
+  List<Servico> loadServicosByUsuario(String usuarioId) {
+    return listEvento.where((servico) => servico.usuario == usuarioId).toList();
+  }
+
+  Future<void> loadServicosFromFirestore() async {
+    try {
+      isLoading = true; // Define isLoading como true ao começar a carregar
+      notifyListeners();
+
+      QuerySnapshot<Map<String, dynamic>> snapshot =
+          await _firestore.collection('Servicos').get();
+      listEvento.clear();
+      for (var element in snapshot.docs) {
+        listEvento.add(Servico.fromMap(element.data()));
+      }
+    } catch (e) {
+      print('Erro ao carregar serviços do Firestore: $e');
+    } finally {
+      isLoading =
+          false; // Define isLoading como false após o carregamento (seja sucesso ou falha)
+      notifyListeners();
+    }
+  }
+
+  void editar(Servico servico) {
+    // Atualiza os dados no Firestore
+    _firestore.collection("Servicos").doc(servico.id).set(servico.toMap());
+
+    notifyListeners();
+  }
+
+  void adiciona(Servico servico) {
+    _firestore.collection("Servicos").doc(servico.id).set(servico.toMap());
+  }
+
+  void remove(Servico servico) {
+    _firestore.collection('Servicos').doc(servico.id).delete();
+    notifyListeners();
+  }
+
+  Future<Servico?> data(String data) async {
+    try {
+      DocumentSnapshot<Map<String, dynamic>> snapshot =
+          await _firestore.collection('Prestadores').doc(data).get();
+
+      if (snapshot.exists) {
+        return Servico.fromMap(snapshot.data()!);
+      } else {
+        return null;
+      }
+    } catch (e) {
+      print('Erro ao carregar cliente: $e');
+      return null;
+    }
+  }
+
+  Future<Servico?> horainicio(String horaInicio) async {
+    try {
+      DocumentSnapshot<Map<String, dynamic>> snapshot =
+          await _firestore.collection('Prestadores').doc(horaInicio).get();
+
+      if (snapshot.exists) {
+        return Servico.fromMap(snapshot.data()!);
+      } else {
+        return null;
+      }
+    } catch (e) {
+      print('Erro ao carregar cliente: $e');
+      return null;
+    }
+  }
+
+  Future<Servico?> horaFim(String horaFim) async {
+    try {
+      DocumentSnapshot<Map<String, dynamic>> snapshot =
+          await _firestore.collection('Prestadores').doc(horaFim).get();
+
+      if (snapshot.exists) {
+        return Servico.fromMap(snapshot.data()!);
+      } else {
+        return null;
+      }
+    } catch (e) {
+      print('Erro ao carregar cliente: $e');
+      return null;
+    }
+  }
+}
